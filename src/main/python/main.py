ad73ecd7f68b6d53815af9601e571c488d622a20
wes <wes@ubuntu-server.localdomain>
2019-08-21 02:02:52 +0000

    initial commit with basic reads and setup with pyb

---
+++ src/main/python/main.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python3
+
+#---------------------------------------------------------#
+#  App Title: Code Samples
+#  File: main.py
+#  Author: Wes Forman
+#  Date: 8/20/2019
+#  Description: This is a code sample that I have written
+#  for job interviews. It will go over various logic, loops
+#  if/else, functions or methods, classes and OOP, system 
+#  calls writing and reading files and running commands and
+#  more.
+#---------------------------------------------------------#
+
+import re
+
+
+#--------------------------Classes------------------------#
+
+# Class User
+class User:
+    
+    Name = "John Doe"                # Default name is John Doe starting out
+    email = "name@something.com"     # email address of user
+    Nickname = "Slick"               # Nickname of user
+
+
+    # constructor of the Character object.
+    def __init__(self, name, email, nickname):
+        self.Name = name
+        self.email = email
+        self.Nickname = nickname
+
+    # Getter Methods
+    def getName(self):
+        return self.Name
+    
+    def getEmail(self):
+        return self.email
+
+    def getNickname(self):
+        return self.Nickname
+
+    # Setter Methods
+
+    def setName(self, name):
+        if( input("Do you really want to change your name Y/n?") == "Y"):
+            self.Name = name
+        else:
+            print("Ok we won't change your name then")
+
+def checkEmail(email):
+    """Checks if the user input a valid email address.
+
+    Takes in a string and uses regex to make sure it is a valid email.
+    
+    Returns boolean True/False"""
+
+    isValid = re.search('^\w+@\w+.\w+$', email)
+    if isValid:
+        return True
+    else:
+        return False
+
+def displayUsers(userList):
+    """Displays a list of the users created.
+
+    Takes in a list of User Objects and outputs them in pretty format.
+    
+    Returns nothing"""
+
+    print("---------------------------------")
+    for u in userList:
+        print("Name = ", u.getName())
+        print("email = ", u.getEmail())
+        print("Nickname = ", u.getNickname())
+        print("---------------------------------")
+
+# Main funciton for code sample
+def main():
+    print("Welcome to Code Sample by Wes Forman\n")
+
+    print("Lets try some user input and logic first\n\n")
+
+    # While loop asking for user input and using try except to make sure we only have ints
+    ans = None
+    while(ans is None):
+        try:
+            ans = int(input("Give me a number between 1 and 10\n"))
+        except ValueError:
+            print("Please only enter a number\n\n")
+        if(ans < 1 or ans > 10):
+            ans = None
+        else:
+            break
+
+    print("Great, you answered with", ans)
+    print("Now I am going to need some more input from you\n")
+    print("Since you chose", ans,", please give me that many names")
+    
+    # Loop through and create a Name list based on the number they gave
+    Name  = []
+    for i in range(0,ans):
+        Name.append(input("Enter a name\n"))
+    
+    print("Alright, lets now get ", ans, " emails, of course fake ones for this exercise\n")
+    
+    # Get email in the correct format from input and store in list
+    Emails = []
+    for i in range(0,ans):
+        # Loop until they enter a valid email then store and go onto the next index
+        while(True):
+            address = input("Enter a valid email. something@yeah.com\n")
+            if(not checkEmail(address)):
+                print("You didn't enter a correct type of email")
+                continue
+            else:
+                break
+        Emails.append(address)
+
+    # Now we will read in one more value from the user as Nickname and store in list in a loop
+    nickname = []
+    print("Now the last thing, lets pick a nick name")
+    
+    for i in range(0,ans):
+        nickname.append(input("Enter a nickname, nothing crazy now..."))
+
+    # Now create user classes based on all that input
+    Usr = []
+    for i in range(0, ans):
+        Usr.append(User(Name[i], Emails[i], nickname[i]))
+
+    print("Now we have ", ans, "Users in a list\n")
+
+    # Method called to display users in a nice format
+    displayUsers(Usr)
+
+
+
+if __name__ == "__main__":
+    main()
